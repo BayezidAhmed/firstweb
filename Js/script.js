c0d32463a67062b2975bbd12dbcdc0f754fd20ad
BayezidAhmed <68195365+BayezidAhmed@users.noreply.github.com>
2022-10-12 23:27:07 +0600

    pepdev

---
+++ Js/script.js
@@ -0,0 +1,49 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:50,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1,
+
+        },
+        1200:{
+            items:1,
+            stagePadding: 350,
+
+        }
+    }
+})
+
+ // fixed nav
+ $(window).scroll(function () {
+    var navScroll = $(window).scrollTop();
+    if (navScroll >= 80) {
+      $("#header").addClass("nav-fixed");
+    } else {
+      $("#header").removeClass("nav-fixed");
+    }
+  });
+
+
+
+
+var barGroup = document.querySelector(".nav-bar-toggler");
+var mobileNav = document.querySelector(".navigation-area");
+var bodyStyle = document.querySelector("body");
+
+barGroup.onclick = function (){
+    bodyStyle.classList.toggle('body-style')
+    barGroup.classList.toggle('click-style')
+    mobileNav.classList.toggle('nav-show')
+  
+}
+
+$(".process-tab .nav-item").hover(function () {
+    $(".process-tab .nav-item").removeClass("active"), $(this).prevAll().addClass("active"), $(this).addClass("active");
+})
